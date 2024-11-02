6d763bb98de92dfd6a0a766a5d1f8e9f63bb31fc
Killian0812 <93117684+Killian0812@users.noreply.github.com>
2024-11-02 22:10:42 +0700

    feat: category

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,7 +1,7 @@
 const Category = require('../models/category.model');
 
-const categories = async (req, res) => {
-    const userId = req.body.userId;
+const getCategories = async (req, res) => {
+    const userId = req.userId;
     Category.find({
         userId: userId,
     }).then(categories => {
@@ -11,4 +11,45 @@ const categories = async (req, res) => {
     })
 }
 
-module.exports = { categories }
+const addCategory = async (req, res) => {
+    const name = req.body.name;
+    const userId = req.userId;
+
+    try {
+        const category = new Category({ name, userId });
+        await category.save();
+        return res.status(201).json(category);
+    } catch (e) {
+        return res.status(500).json({ message: e.message });
+    }
+};
+
+const updateCategory = async (req, res) => {
+    const categoryId = req.params.categoryId;
+    const name = req.body.category;
+
+    try {
+        const category = await Category.findByIdAndUpdate(
+            categoryId, { name }, { new: true }
+        );
+        if (!category) return res.status(404).json({ message: "Category not found" });
+        return res.status(200).json(category);
+    } catch (e) {
+        return res.status(500).json({ message: e.message });
+    }
+};
+
+const deleteCategory = async (req, res) => {
+    const categoryId = req.params.categoryId;
+
+    try {
+        const category = await Category.findByIdAndDelete(categoryId);
+        if (!category) return res.status(404).json({ message: "Category not found" });
+        return res.status(200).json({ message: "Category deleted successfully" });
+    } catch (e) {
+        return res.status(500).json({ message: e.message });
+    }
+};
+
+
+module.exports = { getCategories, addCategory, updateCategory, deleteCategory };
--- routes/product.router.js
+++ routes/product.router.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const productController = require('../controllers/product.controller');
 const verifyJWT = require('../middlewares/verifyJWT');
 
-router.get('/categories', verifyJWT, productController.categories)
+router.get('/categories', verifyJWT, productController.getCategories)
+router.post('/categories', verifyJWT, productController.addCategory);
+router.put('/categories/:categoryId', verifyJWT, productController.updateCategory);
+router.delete('/categories/:categoryId', verifyJWT, productController.deleteCategory);
 
 module.exports = router;
