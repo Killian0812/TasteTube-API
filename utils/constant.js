9dd0c7b2bd2e9204545727c62d1bf115b40d0379
Killian0812 <ngcuong0812@gmail.com>
2024-12-17 18:32:05 +0700

    fix cors

--- utils/constant.js
+++ utils/constant.js
@@ -1,4 +1,4 @@
-const defaultAvatar = 'https://shorturl.at/ajFg5';
+const defaultAvatar = 'https://images2.thanhnien.vn/528068263637045248/2024/4/3/jack-1712114239424422902059.jpg';
 
 module.exports = {
     defaultAvatar
