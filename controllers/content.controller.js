bdd6205bf5fa5d1b760bce9b7ffb43dd7c48e69a
Killian0812 <ngcuong0812@gmail.com>
2024-12-21 18:59:06 +0700

    add new feed api

--- controllers/content.controller.js
+++ controllers/content.controller.js
@@ -1,8 +1,5 @@
 const Video = require("../models/video.model");
-const Product = require("../models/product.model");
 const User = require("../models/user.model");
-const Comment = require("../models/comment.model");
-const VideoLike = require("../models/videoLike.model");
 
 const search = async (req, res) => {
   try {
@@ -36,7 +33,45 @@ const search = async (req, res) => {
 
 const getFeeds = async (req, res) => {
   try {
-    //
+    const feeds = await Video.find({
+      $or: [
+        { visibility: "PUBLIC" },
+        { visibility: "PRIVATE", userId: req.userId },
+      ],
+      userId: { $ne: req.userId }, // Exclude owned videos
+    })
+      .populate({
+        path: "userId",
+        select: "_id username image", // Get id, username and image of owner
+      })
+      .populate({
+        path: "targetUserId",
+        select: "_id username image", // Get id, username and image of target user
+      })
+      .populate({
+        path: "likes",
+        select: "_id userId", // Get id, userId owner
+      })
+      .populate({
+        path: "products",
+        populate: [
+          {
+            path: "category",
+            select: "_id name",
+          },
+          {
+            path: "userId",
+            select: "_id image username phone",
+          },
+        ],
+      });
+
+    const feedsWithUserLiked = feeds.map((video) => ({
+      ...video.toObject(),
+      userLiked: video.likes.some((like) => like.userId.equals(req.userId)),
+    }));
+
+    return res.status(200).json(feedsWithUserLiked);
   } catch (error) {
     return res.status(500).json({ message: error });
   }
