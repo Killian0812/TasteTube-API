04bc8cce647f75d10e83d0ab246f5b573dbf5dc5
Cuong Nguyen <ngcuong0812@gmail.com>
2025-04-01 22:57:45 +0700

    cors

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16mb" }));
 app.use(cookieParser());
 const corsOptions = {
+  origin: ["https://taste-tube.web.app", "http://localhost:5555"],
+  credentials: true,
   exposedHeaders: ["x-auth-token"], // exposed since set-cookie can't be used through ngrok
 };
 app.use(cors(corsOptions));
