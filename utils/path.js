c9269af679d1e9786ba7a94c61c3d580170ecf0e
Killian0812 <93117684+Killian0812@users.noreply.github.com>
2025-04-25 12:16:00 +0700

    change uploadpath on vercel

--- utils/path.js
+++ utils/path.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const rootDir = path.resolve(__dirname, "..");
 const staticFolderPath = path.join(rootDir, "static");
-const uploadPath = path.join(rootDir, "uploads");
+const uploadPath = process.env.VERCEL ? "/tmp" : path.join(rootDir, "uploads");
 
 module.exports = {
   rootDir,
